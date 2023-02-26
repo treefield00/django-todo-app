f9659b9412d4083873d2ec44ddd9bea8d4fb6760
treefield00 <kihara_takashi@me.com>
2023-02-26 17:02:21 +0900

    add author to filter task

--- todo/models.py
+++ todo/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Task(models.Model):
     title = models.CharField("タイトル", max_length=30)
     description = models.TextField("説明", blank=True)
     deadline = models.DateField("締切日")
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        null=True,
+    )
 
     def __str__(self) -> str:
         return self.title
--- todo/views.py
+++ todo/views.py
@@ -8,13 +8,26 @@ from django.views.generic import (
 )
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 
 from todo.models import Task
 
 # Create your views here.
-class TaskListView(ListView):
-    model = Task
+# class TaskListView(ListView):
+#     model = Task
+#     template_name = 'todo/task_list.html'
+
+class TaskListView(LoginRequiredMixin, ListView):
+    paginate_by = 5
     template_name = 'todo/task_list.html'
+
+    def get_queryset(self):
+        current_user = self.request.user
+        if current_user.is_superuser: # スーパーユーザの場合、リストにすべてを表示する。
+            return Task.objects.all()
+        else: # 一般ユーザは自分のレコードのみ表示する。
+            return Task.objects.filter(author=current_user.id)
+
 
 class TaskDetailView(DetailView):
     model = Task
@@ -22,8 +35,14 @@ class TaskDetailView(DetailView):
 
 class TaskCreateView(LoginRequiredMixin, CreateView):
     model = Task
-    fields = '__all__'
+    # fields = '__all__'
+    fields = ['title','description','deadline']
     success_url = reverse_lazy('task-list')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super(CreateView, self).form_valid(form)
+ 
 
 class TaskDeleteView(LoginRequiredMixin, DeleteView):
     model = Task
@@ -31,8 +50,19 @@ class TaskDeleteView(LoginRequiredMixin, DeleteView):
     template_name = 'todo/task_delete.html'
 
 
+# class TaskUpdateView(LoginRequiredMixin, UpdateView):
+#     model = Task
+#     fields = '__all__'
+#     success_url = reverse_lazy('task-list')
+
 class TaskUpdateView(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = '__all__'
+    fields = ['title','description','deadline']
     success_url = reverse_lazy('task-list')
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            raise PermissionDenied('編集権限がありません。')
+        return super(UpdateView, self).dispatch(request, *args, **kwargs)
 
